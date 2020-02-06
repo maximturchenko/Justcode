57a6b9727b2b4452d5e03b8ae665df698d4476f2
Maxi55 <super.practica@yandex.ru>
2020-02-06 19:16:02 +0500

    local

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <?php
-
  
+$n = trim(fgets(STDIN));
 
-require_once __DIR__ . "\app\interfaceSocial.php";
-require_once __DIR__ . "\app\Abstractgetcreator.php";
-require_once __DIR__ . "\app\FacebookCreator.php";
-require_once __DIR__ . "\app\ConcreteFacebook.php";
-require_once __DIR__ . "\app\VkCreator.php";
-require_once __DIR__ . "\app\ConcreteVk.php";
-require_once __DIR__ . "\app\LinkCreator.php";
-require_once __DIR__ . "\app\ConcreteLinkid.php";
- 
-  
-use App\Factorymethod;
+$p=[];
+$l=[];
+while($n>0){
+    $m=explode(" ", trim(fgets(STDIN)));
+    array_push( $p , $m);
+    array_push( $l , NULL);
+    $n--;
+}
+$sum=0;
+for($i=0;$i<count($p);$i++){
+    for($j=0;$j<count($p);$j++){
+        if($p[$i][0]<$p[$j][0] && $p[$i][1]<$p[$j][1]){
+            if(empty($l[$i])){
+                $l[$i]=1;
+            }
+        }        
+    }
+}
 
- 
+for($i=0;$i<count($l);$i++){
+    if(empty($l[$i])){
+        $sum++;
+    }
+}
 
-
-
-
-
-
+echo $sum;
 
 
