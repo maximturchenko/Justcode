f71e90a2fd08ccfd0855f295d1d784e5e35329c0
maximturchenko <maksarik1@yandex.ru>
2020-02-06 11:42:40 +0500

    Start using geat things composer

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
- /* 
-    require_once __DIR__ . ("/app/door/DoorInterface.php");
-    require_once __DIR__ . ("/app/door/MetallicDoor.php");
-    require_once __DIR__ . ("/app/door/WoodenDoor.php");
-    require_once __DIR__ . ("/app/door/MetallicDoorFactory.php");
-    require_once __DIR__ . ("/app/door/WoodenDoorFactory.php");
-*/
+require_once __DIR__ . '../vendor/autoload.php';
+
+use App\door;
+
+$md = new \App\door\MetallicDoor(20,40);
+  
  
